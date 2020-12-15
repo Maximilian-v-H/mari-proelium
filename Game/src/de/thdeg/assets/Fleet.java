2e1ab476651a3a8bfc7c3b30c4e85687054e252f
KliKli2 <maximilian.hohenbuehel@gmail.com>
2020-12-16 00:19:45 +0100

    Added fleet class

---
+++ Game/src/de/thdeg/assets/Fleet.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Fleet {
+    private List<Enemy> fleet;
+
+    Fleet(){
+        this.fleet = new ArrayList<Enemy>();
+    }
+
+    public void addFleetmember(Enemy s){
+        this.fleet.add(s);
+    }
+
+    public void executeOrders(short[] myImage){
+        for (Enemy s : this.fleet){
+            if(s.isAlive()){
+                myImage = s.run(myImage);
+            }
+        }
+    }
+
+
+    public short[] paintFleet(short[] myImage){
+        for (Enemy s : this.fleet) {
+            myImage = s.paint(myImage);
+        }
+        return myImage;
+    }
+}
